1e661f4a5244c97693dd5f2b6093a8a9c6236e08
MrRio21 <mohussien223@gmsil.com>
2023-02-02 23:52:55 -0800

    lab2

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,10 +8,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></head>
 
 <body>
-<div class="container">
-    show {{$post['title']}}
+<div>
+ <div class="card-body">
+    <h5 class="card-title"> <h3>Title:</h3> {{$post['title']}}</h5>
+    <p class="card-text"> <h3>Description:</h3> {{$post['description']}}</p>
+    <p class="card-text">  <h3>Created_at:</h3> {{$post['created_at']}}</p>
+  </div>
 </div>
-
+<br>
+<hr class="container">
+<br>
+<div class="card container">
+  <div class="card-header">
+    Post Creator Info
+  </div>
+  <div class="card-body">
+    <h5 class="card-title"> <h3>Creator:</h3>{{$post['Posted_by']}} </h5>
+  </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
